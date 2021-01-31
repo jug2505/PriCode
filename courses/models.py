ad03229f2676d45193c47c66353d77237f66b8c6
jug2505 <jonkuvshin@gmail.com>
2021-01-31 09:37:57 +0300

    Схема БД

--- courses/models.py
+++ courses/models.py
@@ -1,4 +1,17 @@
 from django.db import models
 
-class Course(models.Model):
-    pass
+
+class Article(models.Model):
+    article_title = models.TextField(default='')
+    article_text = models.TextField(default='')
+
+
+class Question(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    question_text = models.TextField(default='')
+
+
+class Choice(model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    choise_isRight = models.BooleanField(default=False)
