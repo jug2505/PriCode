b1d6527d758341b830ae5ce061437d1fa584ec01
jug2505 <jonkuvshin@gmail.com>
2021-02-03 16:24:54 +0300

    Изменение контроллера показа статьи

--- courses/views.py
+++ courses/views.py
@@ -17,13 +17,18 @@ class ChoiceData:
         self.choise_isRight = choise_isRight
 
 
-class QueData:
-    def __init__(self, questions_text, article_text, article_title, article_id, choices):
-        self.questions_text = questions_text
+class QuestionData:
+    def __init__(self, question_text, choices):
+        self.question_text = question_text
+        self.choices = choices
+
+
+class ArticleData:
+    def __init__(self, questions, article_text, article_title, article_id):
+        self.questions = questions
         self.article_title = article_title
         self.article_text = article_text
         self.article_id = article_id
-        self.choices = choices
 
 
 def home_page(request):
@@ -89,10 +94,6 @@ def article_add(request):
     return redirect('home')
 
 
-def articles(request, course_id):
-    pass
-
-
 def catalog(request):
 
     maxlen = 40  # Длина обрезанной статьи
@@ -117,21 +118,23 @@ def show_article(request, article_id):
     info = Article.objects.get(id=article_id)
     article_title = info.article_title
     article_text = info.article_text
-    questions_text = []
-    choices_data = []
+    questions_data = []
     info_question = Question.objects.all().filter(article=info)
+
     for question in info_question:
         questions_text.append(question.question_text)
         choices = Choice.objects.all().filter(question=question)
+        choices_data = []
         for choice in choices:
             choices_data.append(ChoiceData(
                 choice_text=choice.choice_text, choise_isRight=choice.choise_isRight))
+        
+        questions_data.append(QuestionData(question_text=question.question_text, choices=choices_data))
 
-    data = QueData(
+    data = ArticleData(
         article_id=article_id,
         article_text=article_text,
         article_title=article_title,
-        questions_text=questions_text,
-        choices=choices_data)
+        questions=questions_data,)
 
     return render(request, 'courses/show_article.html', {'article': data})
